7269e44d0a33b36e90310aa85b63f45a2007e533
Katie Harshman <katie@ibm.com>
2022-08-01 15:48:20 -0700

    check all functions

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -50,7 +50,6 @@ const printCells = (state) => {
     };
     board += row.join(" ") + "\n"
   }
-  console.log(board)
   return board
 };
 
@@ -61,17 +60,37 @@ const getNeighborsOf = ([x, y]) => [
 ];
 
 const getLivingNeighbors = (cell, state) => {
-  const bindContains = contains.bind(state)
   return getNeighborsOf(cell).filter(c => contains.bind(state)(c))
 };
 
-const willBeAlive = (cell, state) => {};
+const willBeAlive = (cell, state) => {
+  const neighbors = getLivingNeighbors(cell, state)
+  return neighbors.length === 3 || (contains.call(state, cell) && neighbors.length === 2) 
+};
 
-const calculateNext = (state) => {};
+const calculateNext = (state) => {
+  const { bottomLeft, topRight } = corners(state)
+  let board = []
+  for (let y = topRight[1] + 1; y >= bottomLeft[1] - 1; y-- ) {
+    for (let x = bottomLeft[0] - 1; x <= topRight[0] + 1; x++ ) {
+      board = board.concat(willBeAlive([x, y], state) ? [[x, y]] : [])
+    };
+  }
+  return board
+};
 
-const iterate = (state, iterations) => {};
+const iterate = (state, iterations) => {
+  const states = [state];
+  for (let i = 0; i < iterations; i++) {
+    states.push(calculateNext(states[states.length-1]))
+  }
+  return states
+};
 
-const main = (pattern, iterations) => {};
+const main = (pattern, iterations) => {
+  const result = iterate(startPatterns[pattern], iterations)
+  result.forEach(a => console.log(printCells(a)))
+};
 
 const startPatterns = {
     rpentomino: [
