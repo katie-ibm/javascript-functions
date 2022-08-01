1e9695b59da674d26e10e8fd90a44d2a3290d34c
Katie Harshman <katie@ibm.com>
2022-08-01 11:18:26 -0700

    check thru printCells

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -15,11 +15,15 @@ function same([x, y], [j, k]) {
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-  return this.some(c => some(c, cell));
-
+  return this.some(c => same(c, cell))
 }
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  console.log(`\n-----\n ${cell}\n ${state}`)
+  contains.call(state, cell)
+  console.log(  contains.call(state, cell)  )
+  return contains.call(state, cell) ? '\u25A3' : '\u25A2'
+};
 
 const corners = (state = []) => {};
 
